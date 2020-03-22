52828a0c92f87e0f602a5784f75f5eac27a11ac4
macpp <mac.map.void@gmail.com>
2020-03-22 21:28:47 +0100

    add another example

--- test_2/src/lib.rs
+++ test_2/src/lib.rs
@@ -1,4 +1,5 @@
-
+#![feature(register_attr)]
+#![register_attr(my_attr)]
 #[derive(Debug)]
 pub enum Foo {
     Yes,
@@ -22,5 +23,12 @@ pub fn print(x: Vec<String> )
 {
     for y in x.into_iter() {
         println!("vec element: {}",y);
+    }
+}
+
+pub fn multiple_match(x: i32) {
+    match x {
+        #[my_attr] 0 /*| /*#[my_attr] */12*/ => println!("ok"), // second attribute will not work
+        _ => println!("not ok")
     }
 }
